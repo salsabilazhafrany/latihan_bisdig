15a7cf25bcdee73a0373b6fd2ffcd2c559f40df4
salsabilazhafrany <zhafranysalsabila@gmail.com>
2025-09-10 14:36:55 +0700

    Latihan Java Script

--- latihan.js
+++ latihan.js
@@ -1,9 +1,9 @@
-var number1 = 5 ;
-number1 = 3 ;
-console.log ( number1); // 3
-window.alert ( number1); // 3
-number1 = number1 + 5 ; // 3 + 5 = 8
-number1 = number1 - 6 ; // 8 - 6 = 2
-var number2 = number1 * 10 ; // 2 (times) 10 = 20
-var number3 = number2 / number1; // 20 (divided by) 2 = 10;
-var myString = "I am a " + "string!" ; // "I am a string!"
+alert("Shibal Annyeong!!!");
+alert("lanjut???");
+        let konfirmasi = confirm("yakin mau lajutt?");
+            if (konfirmasi) {
+          let nama = prompt("namamu siapaa dikkk??");
+             alert("Wah, haloo " + nama);
+            }   else {
+             alert("yahhhh, admin kecewa");
+            }
